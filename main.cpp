e6039aeab36b2229fea4852660917a0e77a9d4eb
Gaveno <gavin.monkeystudios@gmail.com>
2016-01-20 17:50:03 -0700

    Removed Comments
    Removed commented lines per instructions.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@
 #include <iostream>
 
 int main() {
-    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-	// Done
     std::cout << "Hello, world! My number is " << 105728013 % 13 << '!' << std::endl;
 
     return 0;
